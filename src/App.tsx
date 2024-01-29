a1b70dde2c8b12a3618206e8aa8ad6fabd58276a
dibborah <dibyajyotiborah469@gmail.com>
2024-01-29 16:29:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+const App = () => {
+  return (
+    <div className="container my-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input type="email" className="form-control" id="email" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
