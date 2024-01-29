ea22a8a9b6e52c9441fcf5749ce6887706594973
dibborah <dibyajyotiborah469@gmail.com>
2024-01-29 19:54:38 +0530

    installed reactHookForm, hookform/resolvers and Joi library . Created validationSchema (passed in resolvers) and given isValid condition on submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
+import { useForm } from "react-hook-form";
+import Joi from "joi";
+import { joiResolver } from "@hookform/resolvers/joi";
+
+const schema = Joi.object({
+  email: Joi.string().min(5).max(200).required().label("Email "),
+  password: Joi.string().min(8).max(1000).required().label("Password"),
+});
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
 const App = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors,isValid },
+  } = useForm<FormData>({
+    resolver: joiResolver(schema),
+  });
   return (
     <div className="container my-5">
-      <form>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
             Email address
           </label>
-          <input type="email" className="form-control" id="email" />
+          <input
+            {...register("email")}
+            type="email"
+            className="form-control"
+            id="email"
+          />
+          {errors.email && (
+            <p className="text-danger">{errors.email.message}</p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            {...register("password")}
+            type="password"
+            className="form-control"
+            id="password"
+          />
+          {errors.password && (
+            <p className="text-danger">{errors.password.message}</p>
+          )}
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button disabled={!isValid} type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
